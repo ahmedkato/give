3bd37ec4fab9302cee632199d327a5f09c632813
Joshua Bechard <josh@trashmountain.com>
2016-05-11 15:02:04 -0500

    Changed the indexes to one 'single' index per declaration

--- server/indexes.js
+++ server/indexes.js
@@ -12,92 +12,63 @@ Config._ensureIndex(
   {'OrgInfo.web.domain_name': 1},
   { unique: true }
 );
-
 Config._ensureIndex(
   { 'Giving.options.id': 1 },
+  { unique: true }
+);
+Config._ensureIndex(
   { 'Giving.options.groupId': 1 },
   { unique: true }
 );
 
-Customers._ensureIndex(
-  {'id': 1},
-  {'customer': 1},
-  {'metadata.dt_persona_id': 1},
-  {background: true});
+Customers._ensureIndex({'id': 1});
+Customers._ensureIndex({'customer': 1});
+Customers._ensureIndex({'metadata.dt_persona_id': 1});
 
-Devices._ensureIndex(
-  {'id': 1},
-  {'customer': 1},
-  {background: true});
+Devices._ensureIndex({'id': 1});
+Devices._ensureIndex({'customer': 1});
 
-Donations._ensureIndex(
-  {'charge_id': 1},
-  {'customer_id': 1},
-  {'method': 1},
-  {'status': 1},
-  {background: true});
+Donations._ensureIndex({'charge_id': 1});
+Donations._ensureIndex({'customer_id': 1});
+Donations._ensureIndex({'method': 1});
+Donations._ensureIndex({'status': 1});
 
-DT_splits._ensureIndex(
-  {memo: "text"},
-  {background: true});
-DT_splits._ensureIndex(
-  {'donation_id': 1},
-  {'fund_id': 1},
-  {background: true});
+DT_splits._ensureIndex({memo: "text"});
+DT_splits._ensureIndex({'donation_id': 1});
+DT_splits._ensureIndex({'fund_id': 1});
 
 DT_donations._ensureIndex({'id': 1});
 DT_donations._ensureIndex({'persona_id': 1});
-DT_donations._ensureIndex({'transaction_id': 1})
+DT_donations._ensureIndex({'transaction_id': 1});
 
-DT_funds._ensureIndex(
-  {'id': 1},
-  {'name': 1},
-  {background: true});
+DT_funds._ensureIndex({'id': 1});
+DT_funds._ensureIndex({'name': 1});
 
-DT_personas._ensureIndex(
-  {'id': 1},
-  {background: true});
+DT_personas._ensureIndex({'id': 1});
 
-Fundraisers._ensureIndex(
-  {'email': 1},
-  { unique: true },
-  { background: true });
+Fundraisers._ensureIndex({'email': 1},{ unique: true });
 
-Invoices._ensureIndex(
-  {'id': 1},
-  {'customer': 1},
-  {'charge': 1},
-  {'subscription': 1},
-  {background: true});
+Invoices._ensureIndex({'id': 1});
+Invoices._ensureIndex({'customer': 1});
+Invoices._ensureIndex({'charge': 1});
+Invoices._ensureIndex({'subscription': 1});
 
-Meteor.users._ensureIndex(
-  {'emails.address': 1},
-  {'primary_customer_id': 1},
-  {'emailSubscriptions.id': 1},
-  {'emailSubscriptions.isActive': 1},
-  {background: true});
+Meteor.users._ensureIndex({'emails.address': 1});
+Meteor.users._ensureIndex({'primary_customer_id': 1});
+Meteor.users._ensureIndex({'emailSubscriptions.id': 1});
+Meteor.users._ensureIndex({'emailSubscriptions.isActive': 1});
 
-Subscriptions._ensureIndex(
-  {'id': 1},
-  {'customer': 1},
-  {background: true});
+Subscriptions._ensureIndex({'id': 1});
+Subscriptions._ensureIndex({'customer': 1});
 
-Transactions._ensureIndex(
-  {'id': 1},
-  {'transfer_id': 1},
-  {background: true});
+Transactions._ensureIndex({'id': 1});
+Transactions._ensureIndex({'transfer_id': 1});
 
-Transfers._ensureIndex(
-  {'id': 1},
-  {'balance_transaction': 1},
-  {background: true});
+Transfers._ensureIndex({'id': 1});
+Transfers._ensureIndex({'balance_transaction': 1});
 
-Trips._ensureIndex(
-  {'fundId': 1},
-  {'name': 1},
-  {background: true});
+Trips._ensureIndex({'fundId': 1});
+Trips._ensureIndex({'name': 1});
 
-Uploads._ensureIndex(
-  {'configId': 1},
-  {'fundId': 1},
-  {background: true});
+Uploads._ensureIndex({'configId': 1});
+Uploads._ensureIndex({'fundId': 1});
