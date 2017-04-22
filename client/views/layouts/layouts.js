c82c604c13193a81c43a1cd400b45f165877a570
JoshJoe <josh@joshjoe.com>
2017-04-21 20:12:54 -0500

    Fixed the favicon loading bug

--- client/views/layouts/layouts.js
+++ client/views/layouts/layouts.js
@@ -1,35 +1,52 @@
 function loadHead() {
-  const config = ConfigDoc();
-
-  if (config && config._id) {
-    const imageDoc = Images.findOne({$and: [{configId: config._id}, {'favicon': "_true"}]});
+  Meteor.setTimeout(()=>{
+    const config = Config.findOne({
+      'OrgInfo.web.domain_name': Meteor.settings.public.org_domain
+    });
+    console.log(config);
+    const imageDoc = Images.findOne({configId: config._id, "meta.favicon": "_true"});
+    console.log(imageDoc);
     if (imageDoc) {
       console.log("Got to imageDoc exists");
-      $('#favicon').attr("href", imageDoc.baseUrl + imageDoc.name);
+      $('#favicon').attr("href", imageDoc.versions.original.meta.pipeFrom + "?v=" + Math.random().toFixed(3) * 1000);
     }
-  }
+  }, 200);
+  Meteor.setTimeout(()=>{
+    const config = Config.findOne({
+      'OrgInfo.web.domain_name': Meteor.settings.public.org_domain
+    });
+    console.log(config);
+    const imageDoc = Images.findOne({configId: config._id, "meta.favicon": "_true"});
+    if (imageDoc) {
+      console.log("Got to imageDoc exists");
+      $('#favicon').attr("href", imageDoc.versions.original.meta.pipeFrom + "?v=" + Math.random().toFixed(3) * 1000);
+    }
+  }, 3000);
 }
 
 Template.MasterLayout.onCreated(function() {
   this.autorun(() => {
-    this.subscribe("config");
+    this.subscribe("config", {onReady() {
+      loadHead();
+    }});
     this.subscribe("uploaded");
   });
-  loadHead();
 });
 
 Template.AdminLayout.onCreated(function() {
   this.autorun(() => {
-    this.subscribe("config");
+    this.subscribe("config", {onReady() {
+      loadHead();
+    }});
     this.subscribe("uploaded");
   });
-  loadHead();
 });
 
 Template.UserLayout.onCreated(function() {
   this.autorun(() => {
-    this.subscribe("config");
+    this.subscribe("config", {onReady() {
+      loadHead();
+    }});
     this.subscribe("uploaded");
   });
-  loadHead();
 });
